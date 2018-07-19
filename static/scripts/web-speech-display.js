7b547d364622940d9c150f0416652487eda7b670
Samsore Shams <samsore_shams@trendmicro.com>
2018-07-19 19:01:30 -0400

    Added Speech Recognition to web-speech-display

--- static/scripts/web-speech-display.js
+++ static/scripts/web-speech-display.js
@@ -3,6 +3,19 @@ AFRAME.registerComponent('web-speech-display', {
     },
   
     init: function () {
+      var recognizer = new webkitSpeechRecognition();
+      recognizer.lang = "en";
+      console.log('init');
+      recognizer.onresult = function(event) {
+        if (event.results.length > 0) {
+            var result = event.results[event.results.length-1];
+            if(result.isFinal) {
+                this.el.setAttribute('text', {value: result[0].transcript});
+                console.log(result[0].transcript);
+            }
+        }
+        recognizer.start();
+      }
     },
   
     tick: function (time, timeDelta) {
