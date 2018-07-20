750cc9908c83b895e44d77df18c0607b181c9fea
Oliver Fei <ofei006@uottawa.ca>
2018-07-20 11:21:00 -0400

    Make text follow player

---
+++ static/scripts/follow-above-object.js
@@ -0,0 +1,19 @@
+
+const RAD_TO_DEGREE = 180/Math.PI;
+
+AFRAME.registerComponent('follow-above-object', {
+    schema: {
+        objectID: {type: 'string', default: ""}
+    },
+    init: function () {
+        this.objectToFollow = document.querySelector("#" + this.data.objectID).object3D;
+    },
+    tick: function () {
+      if (this.objectToFollow != null) {
+          var otherPosition = this.objectToFollow.getWorldPosition();
+          var otherRotation = this.objectToFollow.getWorldRotation();
+          this.el.setAttribute('position', otherPosition.x + " " + (otherPosition.y + 0.2) + " " + otherPosition.z);
+          this.el.setAttribute('rotation', otherRotation.x * RAD_TO_DEGREE + " " + otherRotation.y * RAD_TO_DEGREE + " " + otherRotation.z * RAD_TO_DEGREE);
+      }
+    }
+});
