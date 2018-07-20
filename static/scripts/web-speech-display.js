e61e68f39be6de3a56cc6ebbc2eca891c8ab89b7
Oliver Fei <ofei006@uottawa.ca>
2018-07-20 10:00:44 -0400

    Add update component support. wip tracked controllers

--- static/scripts/web-speech-display.js
+++ static/scripts/web-speech-display.js
@@ -1,23 +1,56 @@
 AFRAME.registerComponent('web-speech-display', {
     schema: {
+      lang: {type: 'string', default: 'en'},
+      interimResults: {type: 'boolean', default: true},
     },
   
     init: function () {
-      var recognizer = new webkitSpeechRecognition();
-      recognizer.lang = "en";
+      easyrtc.enableMicrophone(false);
+      let data = this.data;
+      this.recognizer = new webkitSpeechRecognition();
+      this.recognizer.continuous = true;
+      this.recognizer.interimResults = data.interimResults;
+      this.recognizer.lang = data.lang;
       console.log('init');
-      recognizer.onresult = function(event) {
-        if (event.results.length > 0) {
-            var result = event.results[event.results.length-1];
-            if(result.isFinal) {
-                this.el.setAttribute('text', {value: result[0].transcript});
-                console.log(result[0].transcript);
-            }
-        }
-        recognizer.start();
-      }
+      this.onresult = this.displayResult;
+      this.onend = this.start;
+      this.recognizer.onresult = (event) => {
+        this.onresult(event);
+      };
+      this.recognizer.onend = ()=>{this.onend();};
+      this.recognizer.start();
     },
   
-    tick: function (time, timeDelta) {
+    update: function(previousData) {
+      if(!previousData) return;
+      if (previousData.lang !== this.data.lang) {
+        console.log(previousData.lang);
+        this.recognizer.lang = this.data.lang;
+        this.recognizer.stop();
+      }
+      if (previousData.interimResults !== this.data.interimResults) {
+        console.log(previousData.interimResults);
+        this.recognizer.interimResults = this.data.interimResults;
+        this.recognizer.stop();
+      }
+    },
+
+    remove: function() {
+      this.recognizer.stop()
+    },
+
+    start: function() {
+      console.log('test');
+      this.recognizer.start();
+    },
+
+    displayResult: function(event) {
+      if (event.results.length > 0) {
+          var result = event.results[event.results.length-1];
+          if(result.isFinal) {
+              this.el.setAttribute('text', {value: result[0].transcript});
+              console.log(result[0].transcript);
+          }
+      }
     }
   });
