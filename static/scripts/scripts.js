0eba8af599ccd380f57d88119123da160b6c53c4
Oliver Fei <ofei006@uottawa.ca>
2018-07-28 10:29:00 -0400

    Add language selector

--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -1,6 +1,70 @@
 var url = "https://translate.yandex.net/api/v1.5/tr.json/translate?";
 var keyAPI = "trnsl.1.1.20180720T145714Z.8dca8fdab5b97385.c5fea43191927e8cec35dd294475dbaa79dc17c1";
 
+var langs =
+[['Afrikaans',       ['af-ZA']],
+ ['Bahasa Indonesia',['id-ID']],
+ ['Bahasa Melayu',   ['ms-MY']],
+ ['Català',          ['ca-ES']],
+ ['Čeština',         ['cs-CZ']],
+ ['Deutsch',         ['de-DE']],
+ ['English',         ['en-AU', 'Australia'],
+                     ['en-CA', 'Canada'],
+                     ['en-IN', 'India'],
+                     ['en-NZ', 'New Zealand'],
+                     ['en-ZA', 'South Africa'],
+                     ['en-GB', 'United Kingdom'],
+                     ['en-US', 'United States']],
+ ['Español',         ['es-AR', 'Argentina'],
+                     ['es-BO', 'Bolivia'],
+                     ['es-CL', 'Chile'],
+                     ['es-CO', 'Colombia'],
+                     ['es-CR', 'Costa Rica'],
+                     ['es-EC', 'Ecuador'],
+                     ['es-SV', 'El Salvador'],
+                     ['es-ES', 'España'],
+                     ['es-US', 'Estados Unidos'],
+                     ['es-GT', 'Guatemala'],
+                     ['es-HN', 'Honduras'],
+                     ['es-MX', 'México'],
+                     ['es-NI', 'Nicaragua'],
+                     ['es-PA', 'Panamá'],
+                     ['es-PY', 'Paraguay'],
+                     ['es-PE', 'Perú'],
+                     ['es-PR', 'Puerto Rico'],
+                     ['es-DO', 'República Dominicana'],
+                     ['es-UY', 'Uruguay'],
+                     ['es-VE', 'Venezuela']],
+ ['Euskara',         ['eu-ES']],
+ ['Français',        ['fr-FR']],
+ ['Galego',          ['gl-ES']],
+ ['Hrvatski',        ['hr_HR']],
+ ['IsiZulu',         ['zu-ZA']],
+ ['Íslenska',        ['is-IS']],
+ ['Italiano',        ['it-IT', 'Italia'],
+                     ['it-CH', 'Svizzera']],
+ ['Magyar',          ['hu-HU']],
+ ['Nederlands',      ['nl-NL']],
+ ['Norsk bokmål',    ['nb-NO']],
+ ['Polski',          ['pl-PL']],
+ ['Português',       ['pt-BR', 'Brasil'],
+                     ['pt-PT', 'Portugal']],
+ ['Română',          ['ro-RO']],
+ ['Slovenčina',      ['sk-SK']],
+ ['Suomi',           ['fi-FI']],
+ ['Svenska',         ['sv-SE']],
+ ['Türkçe',          ['tr-TR']],
+ ['български',       ['bg-BG']],
+ ['Pусский',         ['ru-RU']],
+ ['Српски',          ['sr-RS']],
+ ['한국어',            ['ko-KR']],
+ ['中文',             ['cmn-Hans-CN', '普通话 (中国大陆)'],
+                     ['cmn-Hans-HK', '普通话 (香港)'],
+                     ['cmn-Hant-TW', '中文 (台灣)'],
+                     ['yue-Hant-HK', '粵語 (香港)']],
+ ['日本語',           ['ja-JP']],
+ ['Lingua latīna',   ['la']]];
+
 function translateSpeech(event) {
     const webSpeechDisplay = document.querySelector("#text").components['web-speech-display'];
     if (event.results.length > 0) {
@@ -40,7 +104,7 @@ function translateSpeech(event) {
 }
 
 window.onload = function(e) {
-    // console.log('test');
+
     const webSpeechDisplay = document.querySelector("#text").components['web-speech-display'];
     webSpeechDisplay.onresult = (event) => {
         console.log('test');
@@ -57,4 +121,15 @@ window.onload = function(e) {
         console.log('Translating results...');
         translateSpeech(event);
     };
+
+    // console.log('test');
+    const select_language = document.querySelector("#language-select");
+    for (var i = 0; i < langs.length; i++) {
+        select_language.appendChild(new Option(langs[i][0], i));
+    }
+    select_language.value=6;
+    select_language.addEventListener('change', (e) => {
+        console.log(langs[e.target.value][1][0]);
+        document.querySelector('#text').setAttribute('web-speech-display', {lang: langs[e.target.value][1][0]});
+    });
 };
